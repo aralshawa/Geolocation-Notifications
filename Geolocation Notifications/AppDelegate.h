8a4c8a5f7ec623f3205f1e4197631fe2ba68fbe0
Abdul Al-Shawa <abdulalshawa@gmail.com>
2016-07-16 18:04:42 -0400

    Initial Commit

---
+++ Geolocation Notifications/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Geolocation Notifications
+//
+//  Created by Abdul Al-Shawa on 2016-07-16.
+//  Copyright © 2016 Abdul Al-Shawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Geolocation Notifications/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Geolocation Notifications
+//
+//  Created by Abdul Al-Shawa on 2016-07-16.
+//  Copyright © 2016 Abdul Al-Shawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
